4dc9f92cb01780c264638e6d87ca10080a5bcc4c
Tejasmulange64 <tejasmulange64@gmail.com>
2023-10-23 00:26:24 +0530

    Patient Management System Project DSA

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,8 @@
+struct Node* createNode(const char* symptom)
+// {
+//     struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+//     strcpy(node->symptom, symptom);
+//     node->yes = NULL;
+//     node->no = NULL;
+//     return node;
+// }
